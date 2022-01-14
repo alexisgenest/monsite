5fcafd842387e92274b20d4e8bf2dd7e12775a35
Alexis Genest <alexis_genest@hotmail.com>
2022-01-14 09:03:30 -0800

    my updates to fix db

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 import csv
+import os
 
 app = Flask(__name__)
 
@@ -12,14 +13,14 @@ def html_page(page_name):
     return render_template(page_name)
 
 def write_to_file(data):
-    with open('database.txt', mode='a') as database:
+    with open('./db/database.txt', mode='a') as database:
         email = data["email"]
         subject = data["subject"]
         message = data["message"]
         file = database.write(f'\n{email},{subject},{message}')
 
 def write_to_csv(data):
-    with open('database.csv', newline='', mode='a') as database2:
+    with open('./db/database.csv', newline='', mode='a') as database2:
         email = data["email"]
         subject = data["subject"]
         message = data["message"]
